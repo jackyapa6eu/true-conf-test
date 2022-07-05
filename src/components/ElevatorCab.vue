682f9ba26555775f4b3e6573d80d975b03e1a05a
Evgenii Denisov <jackyapa6eu@yandex.ru>
2022-07-05 14:17:43 +0300

    elevator cab added

---
+++ src/components/ElevatorCab.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <span>current level: {{ currentLevel }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ElevatorCab",
+  data() {
+    return {
+      currentLevel: 1
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
